c1f6f670313188b58bd7ff317c4408b0bdf939fb
Prem Parihar <loveprem073@gmail.com>
2017-12-06 22:26:40 +0530

    first commit

---
+++ MissionCode.DesignPatterns/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissionCode.DesignPatterns.SOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var record = new Records();
+            record.add("first Item");
+            record.add("second Item");
+           // record.Save();
+
+          // handles the responsibility of persisting objects here
+            Persistence.Save(record);
+        }
+    }
+}
---
+++ MissionCode.DesignPatterns/SRP.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissionCode.DesignPatterns.SOLID
+{
+    public class Records
+    {
+
+        private List<string> data = new List<string>();
+
+        public void add(string item)
+        {
+            data.Add(item);
+        }
+
+        public string RemoveLastItem()
+        {
+            var lastIndex = data.Count - 1;
+            if (lastIndex < 0)
+            {
+                return null;
+            }
+            var item = data[lastIndex];
+            data.RemoveAt(lastIndex);
+            return item;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, data);
+        }
+
+        // breaks single responsibility principle
+        public void Save()
+        {
+            File.WriteAllText(filePath, ToString());
+            Process.Start(filePath);
+        }
+
+        private string filePath
+        {
+            get
+            {
+                return string.Format("{0}/record.txt", RootDirectory);
+            }
+        }
+        private string RootDirectory
+        {
+            get
+            {
+                return System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            }
+        }
+    }
+
+    // handles the responsibility of persisting objects
+    public class Persistence
+    {
+      
+        public static void Save(Records records)
+        {
+            File.WriteAllText(filePath, records.ToString());
+            Process.Start(filePath);
+        }
+
+        private static string filePath
+        {
+            get
+            {
+                return string.Format("{0}/record.txt", RootDirectory);
+            }
+        }
+        private static string RootDirectory
+        {
+            get
+            {
+                return System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            }
+        }
+    }
+}
