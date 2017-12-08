c50e0b39e54101113ecbd73371cfbc9957536f9a
Prem Parihar <loveprem073@gmail.com>
2017-12-09 02:33:01 +0530

    OCP

---
+++ MissionCode.DesignPatterns/OCP.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MissionCode.DesignPatterns.SOLID
+{
+    public enum Color
+    {
+        Blue, Green, Red
+    }
+
+    public enum Size
+    {
+        Small, Midium, Large
+    }
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string name, Color color, Size size)
+        {
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Color: {Color}, Size: {Size}";
+        }
+    }
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> ByColor(IEnumerable<Product> products, Color color)
+        {
+            return products?.Where(product => product.Color == color);
+        }
+
+        // breaks Open close principle. (every time when you add new filter)
+        public IEnumerable<Product> BySize(IEnumerable<Product> products, Size size)
+        {
+            return products?.Where(product => product.Size == size);
+        }
+    }
+
+    // handles the OCP.
+    public interface IFilter<T> where T : class
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items);
+    }
+
+    public class ColorFilter : IFilter<Product>
+    {
+        private readonly Color _color;
+
+        public ColorFilter(Color color)
+        {
+            this._color = color;
+        }
+
+        public IEnumerable<Product> Filter(IEnumerable<Product> items)
+        {
+            return items?.Where(product => product.Color == _color);
+        }
+    }
+
+    public class SizeFilter : IFilter<Product>
+    {
+        private readonly Size _size;
+
+        public SizeFilter(Size size)
+        {
+            _size = size;
+        }
+
+        public IEnumerable<Product> Filter(IEnumerable<Product> items)
+        {
+            return items?.Where(product => product.Size == _size);
+        }
+    }
+}
--- MissionCode.DesignPatterns/Program.cs
+++ MissionCode.DesignPatterns/Program.cs
@@ -10,13 +10,74 @@ namespace MissionCode.DesignPatterns.SOLID
     {
         static void Main(string[] args)
         {
+
+            SRPExample();
+            OCPExample();
+
+            ExitStatment();
+
+        }
+
+        private static void SRPExample()
+        {
+            WriteHeading("SRP");
             var record = new Records();
             record.Add("first Item");
             record.Add("second Item");
-           // record.Save();
+            // record.Save();
 
-          // handles the responsibility of persisting objects here
+            // handles the responsibility of persisting objects here
             Persistence.Save(record);
+        }
+
+        private static void OCPExample()
+        {
+   
+            WriteHeading("OCP");
+            var productList = new List<Product>
+            {
+                new Product("Shoe",Color.Blue, Size.Midium),
+                new Product("bag",Color.Blue, Size.Large),
+                new Product("Lunch Box",Color.Green, Size.Small),
+                new Product("Hand Riding Gloves",Color.Red, Size.Midium)
+            };
+
+            var filter = new ProductFilter();
+            foreach (var product in filter.ByColor(productList, Color.Blue))
+            {
+                Console.WriteLine(product);
+            }
+            foreach (var product in filter.BySize(productList, Size.Midium))
+            {
+                Console.WriteLine(product);
+            }
+
+            // Extendable filters
+            var colorFilter = new ColorFilter(Color.Blue);
+            foreach (var product in colorFilter.Filter(productList))
+            {
+                Console.WriteLine(product);
+            }
+            var sizeFilter = new SizeFilter(Size.Midium);
+            foreach (var product in sizeFilter.Filter(productList))
+            {
+                Console.WriteLine(product);
+            }
+       
+        }
+
+        static void WriteHeading(string heading)
+        {
+            Console.WriteLine("============================================");
+            Console.WriteLine(heading);
+            Console.WriteLine("============================================");
+           
+        }
+
+        static void ExitStatment()
+        {
+            Console.WriteLine("Press any key to exit!!");
+            Console.ReadKey();
         }
     }
 }
