1d35286cd7aaab1a51e90573221c2673ffd2a2cb
Prem Parihar <loveprem073@gmail.com>
2017-12-09 17:20:27 +0530

    LSP

---
+++ MissionCode.DesignPatterns/LSP.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace MissionCode.DesignPatterns.SOLID
+{
+    public abstract class WidgetBase
+    {
+        public virtual WidgetBase[] Relatives()
+        {
+            return new WidgetBase[0];
+        }
+    }
+
+    public class NotLspWidget : WidgetBase
+    {
+        public override WidgetBase[] Relatives()
+        {
+            return new NotLspWidget[1] { this };
+        }
+    }
+
+    public class LspWidget : WidgetBase
+    {
+        public override WidgetBase[] Relatives()
+        {
+            return new WidgetBase[1] { this };
+        }
+    }
+}
---
+++ MissionCode.DesignPatterns/LSPTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace MissionCode.DesignPatterns.SOLID
+{
+    // ReSharper disable once InconsistentNaming
+    [TestClass]
+    public class LSPTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArrayTypeMismatchException))]
+        public void LspCovariance_ThrowException()
+        {
+            WidgetBase w = new NotLspWidget();
+            WidgetBase[] relatives = w.Relatives();
+
+            //try to put some other WidgetBase to the array
+            relatives[0] = new LspWidget();
+
+            Assert.Fail("ArrayTypeMismatchException exception should have been thrown");
+        }
+        [TestMethod]
+        public void LspCovariance_Works()
+        {
+            WidgetBase w = new LspWidget();
+            WidgetBase[] relatives = w.Relatives();
+
+            //try to put some other WidgetBase to the array
+            relatives[0] = new NotLspWidget();
+            Assert.IsTrue(true);
+        }
+    }
+}
