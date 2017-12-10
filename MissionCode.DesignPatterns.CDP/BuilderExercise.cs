241d79236c66f431fc2c5d40b0d2793c7f6bd0b1
Prem Parihar <loveprem073@gmail.com>
2017-12-10 12:10:44 +0530

    Builder Summary

--- MissionCode.DesignPatterns.CDP/BuilderExercise.cs
+++ MissionCode.DesignPatterns.CDP/BuilderExercise.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace MissionCode.DesignPatterns.CDP
 {
+    /// <summary>
+    /// Builder Design Pattern
+    /// A builder is a separate component for building an object
+    /// Can either give builder a constructor or return it via a static function
+    /// To Make builder fluent, return this
+    /// </summary>
     public class CodeBuilder
     {
         private Class _class;
