a7299cfff064aec1a10b20a958ce084ceac8378c
Prem Parihar <loveprem073@gmail.com>
2017-12-10 23:26:25 +0530

    AbstractFactory

--- MissionCode.DesignPatterns.Creational/Program.cs
+++ MissionCode.DesignPatterns.Creational/Program.cs
@@ -10,8 +10,24 @@ namespace MissionCode.DesignPatterns.Creational
     {
         static void Main(string[] args)
         {
+            Builder();
+            AbstractFactory();
+        }
+
+        private static void Builder()
+        {
             var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
             Console.WriteLine(cb);
+        }
+
+        static void AbstractFactory()
+        {
+            var machine = new HotDrinkMachine();
+            //var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 300);
+            //drink.Consume();
+
+            IHotDrink drink = machine.MakeDrink();
+            drink.Consume();
         }
     }
 
