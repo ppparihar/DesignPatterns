ca1cc2665d318a7d546465f1c8563f075665c1c8
Prem Parihar <loveprem073@gmail.com>
2017-12-10 23:12:20 +0530

    Factory Design Principle

---
+++ MissionCode.DesignPatterns.Creational/Factory.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissionCode.DesignPatterns.Creational
+{
+    public class Point
+    {
+        private double x, y;
+
+        protected Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point(double a,
+          double b, // names do not communicate intent
+          CoordinateSystem cs = CoordinateSystem.Cartesian)
+        {
+            switch (cs)
+            {
+                case CoordinateSystem.Polar:
+                    x = a * Math.Cos(b);
+                    y = a * Math.Sin(b);
+                    break;
+                default:
+                    x = a;
+                    y = b;
+                    break;
+            }
+
+            // steps to add a new system
+            // 1. augment CoordinateSystem
+            // 2. change ctor
+            // It breakes the Single responsibility principle 
+        }
+
+
+        // factory method
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        public override string ToString()
+        {
+            return $"X: {x}, Y: {y}";
+        }
+
+        public enum CoordinateSystem
+        {
+            Cartesian,
+            Polar
+        }
+
+        // make it lazy
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+    }
+
+    public class PointFactory
+    {
+        public static Point NewCartesianPoint(float x, float y)
+        {
+            return new Point(x, y); // needs to be public and client can create a instance of Point without calling PointFactory
+
+        }
+    }
+}
---
+++ MissionCode.DesignPatterns.Creational/FactoryTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MissionCode.DesignPatterns.Creational
+{
+    [TestClass]
+    public class FactoryTest
+    {
+        [TestMethod]
+        public void Point_InstantiateByCallingSimpleConstructor_Works()
+        {
+            var point = new Point(1, 2);
+            Assert.IsTrue(string.Equals(point.ToString(), "X: 1, Y: 2"));
+        }
+
+        [TestMethod]
+        public void Point_InstantiateByCallingOverloadConstructor_Works()
+        {
+            var rho = 30;
+            var theta = 30;
+
+            var x = rho * Math.Cos(theta);
+            var y = rho * Math.Sin(theta);
+
+            var expected = $"X: {x}, Y: {y}";
+            var point = new Point(rho, theta, Point.CoordinateSystem.Polar);
+
+            Assert.IsTrue(string.Equals(point.ToString(), expected));
+        }
+
+        [TestMethod]
+        public void Point_InstantiateByFactoryMethod_Works()
+        {
+            var rho = 30;
+            var theta = 30;
+
+            var x = rho * Math.Cos(theta);
+            var y = rho * Math.Sin(theta);
+
+            var expected = $"X: {x}, Y: {y}";
+            var point = Point.NewPolarPoint(rho, theta);
+
+            Assert.IsTrue(string.Equals(point.ToString(), expected));
+        }
+
+        [TestMethod]
+        public void Point_InstantiateByInnerFactory_Works()
+        {
+            var x = 10;
+            var y = 20;
+
+            var expected = $"X: {x}, Y: {y}";
+            var point = Point.Factory.NewCartesianPoint(x, y);
+
+            Assert.IsTrue(string.Equals(point.ToString(), expected));
+        }
+    }
+}
