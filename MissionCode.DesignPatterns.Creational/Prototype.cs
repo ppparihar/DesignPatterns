2acbdab767b35d66bfeeced4cfe2ab1727721dd6
Prem Parihar <loveprem073@gmail.com>
2017-12-11 00:25:33 +0530

    Prototype

---
+++ MissionCode.DesignPatterns.Creational/Prototype.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissionCode.DesignPatterns.Creational
+{
+    public class Coordinate
+    {
+        public int X, Y;
+
+        public Coordinate DeepCopy()
+        {
+            return new Coordinate
+            {
+                X = X,
+                Y = Y
+            };
+
+        }
+    }
+
+    public class Line
+    {
+        public Coordinate Start, End;
+
+        public Line DeepCopy()
+        {
+            var copy = new Line { Start = Start.DeepCopy(), End = End.DeepCopy() };
+            return copy;
+        }
+    }
+}
---
+++ MissionCode.DesignPatterns.Creational/PrototypeTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MissionCode.DesignPatterns.Creational
+{
+    [TestClass]
+    public class PrototypeTest
+    {
+        [TestMethod]
+        public void DeepCopy_Works()
+        {
+            var line1 = new Line
+            {
+                Start = new Coordinate { X = 1, Y = 1 },
+                End = new Coordinate { X = 8, Y = 7 }
+            };
+
+            var line2 = line1.DeepCopy();
+
+            line1.Start.X = line1.Start.Y = line1.End.X = line1.End.Y = 0;
+
+            Assert.AreEqual(line2.Start.X, 1);
+            Assert.AreEqual(line2.Start.Y, 1);
+            Assert.AreEqual(line2.End.X, 8);
+            Assert.AreEqual(line2.End.Y, 7);
+
+        }
+    }
+}
