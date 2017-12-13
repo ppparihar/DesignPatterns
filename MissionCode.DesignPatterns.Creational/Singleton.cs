e7ecc2b81623b27ac6bce4cdbcb75fa3d3f5da2e
Prem Parihar <loveprem073@gmail.com>
2017-12-13 23:17:25 +0530

    singleton

---
+++ MissionCode.DesignPatterns.Creational/Singleton.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using MoreLinq;
+
+
+namespace MissionCode.DesignPatterns.Creational
+{
+    public interface IDatabase
+    {
+        int GetProducts(string name);
+    }
+
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> products;
+        private static int instanceCount;
+        public static int Count => instanceCount;
+
+        private SingletonDatabase()
+        {
+
+
+            products = File.ReadAllLines(
+              Path.Combine(
+                new FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName, "products.txt")
+              )
+              .Batch(2)
+              .ToDictionary(
+                list => list.ElementAt(0).Trim(),
+                list => int.Parse(list.ElementAt(1)));
+        }
+
+        public int GetProducts(string name)
+        {
+            return products[name];
+        }
+
+        // laziness + thread safety
+        private static Lazy<SingletonDatabase> instance = new Lazy<SingletonDatabase>(() =>
+        {
+            instanceCount++;
+            return new SingletonDatabase();
+        });
+
+        public static IDatabase Instance => instance.Value;
+    }
+
+    public class SingletonRecordFinder
+    {
+        public int TotalProducts(IEnumerable<string> names)
+        {
+            int result = 0;
+            foreach (var name in names)
+            {
+                // SingletonRecordFinder is tightly coupled with SingletonDatabase
+                // Which force to test with the actual implementation(live data) 
+                result += SingletonDatabase.Instance.GetProducts(name);
+            }
+
+            return result;
+        }
+    }
+
+    public class ConfigurableRecordFinder
+    {
+        private IDatabase database;
+
+        public ConfigurableRecordFinder(IDatabase database)
+        {
+            this.database = database;
+        }
+
+        public int GetTotalProducts(IEnumerable<string> names)
+        {
+            int result = 0;
+            foreach (var name in names)
+                result += database.GetProducts(name);
+            return result;
+        }
+    }
+
+    //Constructor has to be public, so that we can instancate the object and inject it.
+    //We can use DI framwork for depencency injection 
+    public class DummyDatabase : IDatabase
+    {
+        public int GetProducts(string name)
+        {
+            return new Dictionary<string, int>
+            {
+                ["alpha"] = 1,
+                ["beta"] = 2,
+                ["gamma"] = 3
+            }[name];
+        }
+    }
+
+}
---
+++ MissionCode.DesignPatterns.Creational/SingletonTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MissionCode.DesignPatterns.Creational
+{
+    [TestClass]
+    public class SingletonTests
+    {
+        [TestMethod]
+        public void IsSingletonTest()
+        {
+            var db = SingletonDatabase.Instance;
+            var db2 = SingletonDatabase.Instance;
+            Assert.AreSame(db, db2);
+            Assert.AreEqual(SingletonDatabase.Count, 1);
+        }
+
+        [TestMethod]
+        public void SingletonTotalProductTest()
+        {
+            // testing on a live database
+            var rf = new SingletonRecordFinder();
+            var names = new[] { "Pen", "Book" };
+            int tp = rf.TotalProducts(names);
+            Assert.AreEqual(tp, 3200 + 2500);
+        }
+
+        [TestMethod]
+        public void DependantTotalProductsTest()
+        {
+            var db = new DummyDatabase();
+            var rf = new ConfigurableRecordFinder(db);
+            Assert.AreEqual(rf.GetTotalProducts(new[] { "alpha", "gamma" }), 4);
+        }
+    }
+
+
+}
