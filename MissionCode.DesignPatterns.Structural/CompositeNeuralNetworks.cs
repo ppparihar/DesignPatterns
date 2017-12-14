8c05392a321436674d9b792642097e579947602a
Prem Parihar <loveprem073@gmail.com>
2017-12-15 01:21:15 +0530

    CompositeNeuralNetworks

---
+++ MissionCode.DesignPatterns.Structural/CompositeNeuralNetworks.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissionCode.DesignPatterns.Structural
+{
+    // we cannot use a base class 
+
+    public static class NeuronExtensionMethods
+    {
+        public static void ConnectTo(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
+        {
+            if (ReferenceEquals(self, other)) return;
+
+            foreach (var from in self)
+                foreach (var to in other)
+                {
+                    from.Out.Add(to);
+                    to.In.Add(from);
+                }
+        }
+    }
+
+    public class Neuron : IEnumerable<Neuron>
+    {
+        public float Value;
+        public List<Neuron> In, Out;
+
+        public Neuron()
+        {
+            In = new List<Neuron>();
+            Out = new List<Neuron>();
+        }
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            yield return this;
+        }
+    }
+
+    public class NeuronLayer : Collection<Neuron>
+    {
+
+    }
+
+
+}
--- MissionCode.DesignPatterns.Structural/Program.cs
+++ MissionCode.DesignPatterns.Structural/Program.cs
@@ -10,6 +10,19 @@ namespace MissionCode.DesignPatterns.Structural
     {
         static void Main(string[] args)
         {
+            Composite();
+        }
+
+        static void Composite()
+        {
+            var neuron1 = new Neuron();
+            var neuron2 = new Neuron();
+            var layer1 = new NeuronLayer();
+            var layer2 = new NeuronLayer();
+
+            neuron1.ConnectTo(neuron2);
+            neuron1.ConnectTo(layer1);
+            layer1.ConnectTo(layer2);
         }
     }
 }
