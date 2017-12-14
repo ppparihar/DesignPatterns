bb985ce67dd2ac099aa4e4c310fed9a27ff49d93
Prem Parihar <loveprem073@gmail.com>
2017-12-15 01:14:01 +0530

    Composite

---
+++ MissionCode.DesignPatterns.Structural/Composite.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissionCode.DesignPatterns.Structural
+{
+    //Composite design patterns
+    //Objects can use other objects via inheritance/composition
+    //Some composed and singular objects need similar/identical behaviors
+    //Composite design pattern lets us treat both types of objects uniformly
+    public interface IValueContainer:IEnumerable<int>
+    {
+
+    }
+
+    public class SingleValue : IValueContainer
+    {
+        public int Value;
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return Value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class ManyValues : List<int>, IValueContainer
+    {
+
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Sum(this List<IValueContainer> containers)
+        {
+            int result = 0;
+            foreach (var c in containers)
+                foreach (var i in c)
+                    result += i;
+            return result;
+        }
+    }
+}
---
+++ MissionCode.DesignPatterns.Structural/CompositeTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MissionCode.DesignPatterns.Structural
+{
+    [TestClass]
+    public class CompositeTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var singleValue = new SingleValue { Value = 11 };
+            var otherValues = new ManyValues();
+            otherValues.Add(22);
+            otherValues.Add(33);
+            Assert.AreEqual(new List<IValueContainer> { singleValue, otherValues }.Sum(), 66);
+        }
+    }
+}
